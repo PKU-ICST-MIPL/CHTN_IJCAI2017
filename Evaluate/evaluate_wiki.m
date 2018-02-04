c07887918995ac9d4306f2a2871e7debc1068841
PKU-ICST-MIPL <35952654+PKU-ICST-MIPL@users.noreply.github.com>
2018-02-04 13:54:46 +0800

    Add files via upload

---
+++ Evaluate/evaluate_wiki.m
@@ -0,0 +1,29 @@
+%% ----------Initialization------------
+
+
+load('Label.mat'); %Load labels of test set as teCatAll. Make sure Label.mat matches the label of your test set!
+
+
+
+te_n_I = size(teCatAll,1);
+te_n_T = size(teCatAll,1);
+teImgCat = teCatAll;
+teTxtCat = teCatAll;
+
+%% -------------------Search Task Definition(Wikipedia)-----------------------
+I_te = importdata('img_prob/feature.txt'); %Load Img shared representation
+T_te = importdata('txt_prob/feature.txt'); %Load Txt shared representation
+
+
+
+
+disp('Test');
+D = pdist2(I_te, T_te, 'cosine');
+Z = -D;
+
+% %Image->Text
+QryonTestBi(Z, teImgCat, teTxtCat);
+% %Text->Image
+QryonTestBi(Z', teTxtCat, teImgCat);
+
+
